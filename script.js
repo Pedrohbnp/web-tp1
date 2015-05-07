5e824f45586f102999a04bd6fdd03e27030f9282
Pedrohbnp <pedrohbnprado@hotmail.com>
2015-05-07 19:48:25 -0300

    Commit4

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
 
 function show(e){
 	console.log(e);
+	document.getElementById('BackContent').style.height = "100%";
 	document.getElementById('Content').style.visibility = "hidden";
 	document.getElementById('BackContent').style.visibility = "hidden";
 	document.getElementById('About').style.display = "none";
@@ -16,6 +17,7 @@ function show(e){
 	document.getElementById('Projects').style.display = "none";
 	document.getElementById('Contact').style.display = "none";
 	document.getElementById('Deadpool').style.display = "none";
+	var cont = document.getElementById('Content')
 
 	if(e.target.innerText == 'Inicio'){
 		document.getElementById('Content').style.visibility = "hidden";
@@ -28,6 +30,12 @@ function show(e){
 			document.getElementById('About').style.display = "none";
 		else
 			document.getElementById('About').style.display = "block";
+
+		if(cont.scrollHeight > cont.clientHeight || cont.scrollWidth > cont.clientWidth){
+			var alturaAtual = document.getElementById('About').offsetHeight;
+			console.log(alturaAtual)
+			document.getElementById('BackContent').style.height = alturaAtual+40;
+		}
 	}
 	else if(e.target.innerText == 'Curriculo'){
 		document.getElementById('Content').style.visibility = "visible";
@@ -36,6 +44,11 @@ function show(e){
 			document.getElementById('CV').style.display = "none";
 		else
 			document.getElementById('CV').style.display = "block";
+		if(cont.scrollHeight > cont.clientHeight || cont.scrollWidth > cont.clientWidth){
+			var alturaAtual = document.getElementById('CV').offsetHeight;
+			console.log(alturaAtual)
+			document.getElementById('BackContent').style.height = alturaAtual+40;
+		}
 	}
 	else if(e.target.innerText == 'Projetos'){
 		document.getElementById('Content').style.visibility = "visible";
@@ -44,6 +57,11 @@ function show(e){
 			document.getElementById('Projects').style.display = "none";
 		else
 			document.getElementById('Projects').style.display = "block";
+		if(cont.scrollHeight > cont.clientHeight || cont.scrollWidth > cont.clientWidth){
+			var alturaAtual = document.getElementById('Projects').offsetHeight;
+			console.log(alturaAtual)
+			document.getElementById('BackContent').style.height = alturaAtual+40;
+		}
 	}
 	else if(e.target.innerText == 'Contato'){
 		document.getElementById('Content').style.visibility = "visible";
@@ -52,6 +70,11 @@ function show(e){
 			document.getElementById('Contact').style.display = "none";
 		else
 			document.getElementById('Contact').style.display = "block";
+		if(cont.scrollHeight > cont.clientHeight || cont.scrollWidth > cont.clientWidth){
+			var alturaAtual = document.getElementById('Contact').offsetHeight;
+			console.log(alturaAtual)
+			document.getElementById('BackContent').style.height = alturaAtual+40;
+		}
 	}
 	else if(e.target.nodeName == 'IMG'){
 		document.getElementById('Content').style.visibility = "visible";
@@ -60,13 +83,31 @@ function show(e){
 			document.getElementById('Deadpool').style.display = "none";
 		else
 			document.getElementById('Deadpool').style.display = "block";
+		if(cont.scrollHeight > cont.clientHeight || cont.scrollWidth > cont.clientWidth){
+			var alturaAtual = document.getElementById('Deadpool').offsetHeight;
+			console.log(alturaAtual)
+			document.getElementById('BackContent').style.height = alturaAtual+40;
+		}
 	}
+}
 
-	
+function lightbox(l){
+	console.log(1111);
+	console.log(l);
+
+	 if(l.target.attributes.nodeValue == 'AboutMe.png'){
+	 	console.log(1234);
+	 	document.getElementById('lightbox_content').innerHTML = '<img src="AboutMe.png">';
+	 }
+
 }
 
 var b = document.getElementsByTagName("button");
 console.log(b.length);
- for (var i = 0; i < b.length; i++) {
-    b[i].addEventListener('click', show, false);
- }
+for (var i = 0; i < b.length; i++) {
+	if(b[i].id == 'buttonBox'){
+		b[i].addEventListener('click', lightbox, false);
+	}
+	else
+		b[i].addEventListener('click', show, false);
+}
